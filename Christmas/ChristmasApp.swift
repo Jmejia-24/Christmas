8290239cd8b0926b554b21e92b6401d5efc56602
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-12-31 12:43:11 -0600

    Initial Commit

---
+++ Christmas/ChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasApp.swift
+//  Christmas
+//
+//  Created by Byron Mejia on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
